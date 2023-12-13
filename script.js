422bf28db79592c1702bbe701e9e37eb17284490
Michaelhenri <michaeldt2015@gmail.com>
2023-12-13 20:31:51 -0300

    implementado um <p> para resultado do sorteio, tirando o alert

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
+    const numDrawn = document.querySelector('.number-drawn')
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    numDrawn.innerHTML = result;
+
+   // alert(result)
 }
