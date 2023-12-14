5addb03135b8454a74ef481587aa3f03ffef53db
Michaelhenri <michaeldt2015@gmail.com>
2023-12-13 21:15:46 -0300

    modificação no h2, alterando texto com JS

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
     const numDrawn = document.querySelector('.number-drawn')
+    const textNumDrawn = document.querySelector('.text-draw-h2')
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     numDrawn.innerHTML = result;
+    textNumDrawn.innerHTML = "Número Sorteado"
 
-   // alert(result)
+
+    // alert(result)
 }
